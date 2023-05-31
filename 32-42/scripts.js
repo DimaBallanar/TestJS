219c6bcc1c1c1f813a3c5bda288e12f420e81a14
Dima Ballanar <D.Ballanar@gmail.com>
2023-05-31 08:32:32 +0300

    33

--- 32-42/scripts.js
+++ 32-42/scripts.js
@@ -2,12 +2,24 @@
 // элемент к нижнему регистру. Повторить строку 3 раза. Добавить проверку на
 // ввод только текстовых значений
 
-const text=prompt();
-if(!isNaN(text)){
-    alert('error');
-}
-else{
-    for(i=1;i<4;i++){
-    alert(text.toLowerCase().trim());
-    }
-}
+// const text=prompt();
+// if(!isNaN(text)){
+//     alert('error');
+// }
+// else{
+//     for(i=1;i<4;i++){
+//     alert(text.toLowerCase().trim());
+//     }
+// }
+
+
+// 33. Дана строка из 3 слов разделенных пробелом:
+// • Найдите количество символов в этой строке.
+// • Вырежите из нее 2 и 3 слова.
+// • Найдите индекс 2 подстроки
+
+// const text='rtyerre rw werwrc';
+// console.log(text.length);
+// let text1=text.split(' ')[0];
+// console.log(text1);
+// console.log(text.indexOf(text.split(' ')[1]));
