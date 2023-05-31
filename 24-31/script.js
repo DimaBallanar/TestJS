8efefa4847bae80dd55b3432e1ddb5c020ca6b29
Dima Ballanar <D.Ballanar@gmail.com>
2023-05-31 07:20:26 +0300

    25

--- 24-31/script.js
+++ 24-31/script.js
@@ -7,10 +7,16 @@
 // 25. Ввести строку. Проверить на isNaN. Если число, то ошибка ввода. Если это строка
 // то узнать какая у нее длина. Привести к одному регистру, убрать лишние пробелы
 
-const text=prompt().trim();
-if(!isNaN(text)){
-    alert('error')
-}
-else{
-    alert(`у строки ${text.toLowerCase()} ${text.length} символов`);
-}
+// const text=prompt().trim();
+// if(!isNaN(text)){
+//     alert('error')
+// }
+// else{
+//     alert(`у строки ${text.toLowerCase()} ${text.length} символов`);
+// }
+
+
+// 26. ‘HTML JavaScript PHP’ преобразовать в "HTML-JAVASCRIPT-PHP"
+ 
+const text='HTML JavaScript PHP';
+alert(text.replaceAll(' ','-'))
