4d4735dd726ba8f0267643c120587d1f780b1172
Dima Ballanar <D.Ballanar@gmail.com>
2023-05-31 07:24:43 +0300

    27

--- 24-31/script.js
+++ 24-31/script.js
@@ -9,7 +9,7 @@
 
 // const text=prompt().trim();
 // if(!isNaN(text)){
-//     alert('error')
+//     alert('error');
 // }
 // else{
 //     alert(`у строки ${text.toLowerCase()} ${text.length} символов`);
@@ -18,5 +18,17 @@
 
 // 26. ‘HTML JavaScript PHP’ преобразовать в "HTML-JAVASCRIPT-PHP"
  
-const text='HTML JavaScript PHP';
-alert(text.replaceAll(' ','-'))
+// const text='HTML JavaScript PHP';
+// alert(text.replaceAll(' ','-'));
+
+// 27. Ввести строку. Проверить является ли это число или строка. Если число, то
+// ошибка ввода. Если это строка то первый символ поставить в верхний регистр.
+// Убрать лишние пробелы
+
+// const text=prompt().trim();
+// if(!isNaN(text)){
+//     alert('error');
+// }
+// else{
+//     alert(`${text[0].toUpperCase() + text.slice(1)}`)
+// }
