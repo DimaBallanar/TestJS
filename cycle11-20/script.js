c75b20033d1bc0d14c46b3e790e1f22ccf2984f3
Dima Ballanar <D.Ballanar@gmail.com>
2023-06-01 17:21:53 +0300

    12 cycle

--- cycle11-20/script.js
+++ cycle11-20/script.js
@@ -16,10 +16,20 @@
 // }
 
 // 12. Найдите минимальное значение в массиве используя цикл
-const array=[1,2,3,4,5,6,7,2,34,2542,3,12,312,4412,21,2131,31,414,12211,3,-88];
-let i=0;
-array.forEach(element => {
-    if(i>=element){i=element}    
-});
+// const array=[1,2,3,4,5,6,7,2,34,2542,3,12,312,4412,21,2131,31,414,12211,3,-88];
+// let i=0;
+// array.forEach(element => {
+//     if(i>=element){i=element}    
+// });
 
-alert(i);
+// alert(i);
+
+// 13. Дан массив с элементами [2, 3, 4, 5]. С помощью цикла for найдите произведение
+// элементов этого массива
+
+const array=[2,3,4,5];
+let result=1;
+for(let i=0;i<array.length;i++){
+    result*=array[i];
+}
+alert(result);
