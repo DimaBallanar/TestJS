338429b40e011696e75693f026c5ea02bbff7b27
Dima Ballanar <D.Ballanar@gmail.com>
2023-06-15 22:55:30 +0300

    task1.js

---
+++ DaiLy1506/scripts/task1.js
@@ -0,0 +1,30 @@
+
+// function Checka(text) {
+//     const glas = ["A", "E", "I", "O", "U", "Y"];
+//     let countS = 0;
+   
+//     for (let i = 0; text.length; i++) {
+//         if (glas.includes(text[i].toUpper())) {
+//             countS++;
+//         }
+//     }
+//     return countS;
+// }
+
+// function CheckB(text) {
+//     const soglas = ['B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'X', 'Y', 'Z'];
+//     let countG = 0;
+//  for (let i = 0; text.length; i++) {
+//         if (glas.includes(text[i].toUpper())) {
+//             countG++;
+//         }
+//     }
+//     return countG;
+// }
+// const button = document.querySelector("button");
+// button.addEventListener("click", function(){
+// const inputText = document.querySelector(".input1");
+// const result=document.querySelector("span");
+// result.textContent=`гласных ${Checka(inputText.value)} согласных ${CheckB(inputText.value)}`;
+// });
+ 
